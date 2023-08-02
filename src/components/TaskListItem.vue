b8aa00b78a9207075e085d44966ce62675634d9b
alen <fation.qama.god@gmail.com>
2023-08-02 03:58:01 -0700

    feat: add tasklistitem component

---
+++ src/components/TaskListItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+    <td class="px-6 py-4">1</td>
+    <th
+      scope="row"
+      class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+    >
+      Apple MacBook Pro 17"
+    </th>
+    <td class="px-6 py-4">Silver</td>
+    <td class="px-6 py-4">Laptop</td>
+    <td class="px-6 py-4">
+      <span class="w-6 h-6 text-gray-800 dark:text-white">
+        <i class="fa fa-edit"></i>
+      </span>
+      <span class="w-6 h-6 text-gray-800 dark:text-white">
+        <i class="fa fa-trash"></i>
+      </span>
+    </td>
+  </tr>
+</template>
