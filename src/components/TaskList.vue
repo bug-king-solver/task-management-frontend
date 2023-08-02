2378cb6e0415ff38a5d33f8d5b97216c78d26a4d
alen <fation.qama.god@gmail.com>
2023-08-02 04:51:16 -0700

    feat: add TaskList component

---
+++ src/components/TaskList.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="contatiner">
+    <div>
+      <button
+        type="button"
+        class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+      >
+        Create Task
+      </button>
+    </div>
+    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+      <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead
+          class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
+        >
+          <tr>
+            <th scope="col" class="px-6 py-3">ID</th>
+            <th scope="col" class="px-6 py-3">Task</th>
+            <th scope="col" class="px-6 py-3">Created By</th>
+            <th scope="col" class="px-6 py-3">Assigned To</th>
+            <th scope="col" class="px-6 py-3">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <task-list-item />
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import TaskListItem from "./TaskListItem.vue";
+export default defineComponent({
+  name: "TaskList",
+  components: { TaskListItem },
+});
+</script>
