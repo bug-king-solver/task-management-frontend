09391d43413e03dddb86649eec944a7f4217d4ac
alen <fation.qama.god@gmail.com>
2023-08-03 03:13:54 -0700

    stylel: change position of create button

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -4,7 +4,7 @@
       <div class="mt-10 mb-10">
         <p class="text-center text-2xl">Task Management</p>
       </div>
-      <div class="flex justify-center mb-6">
+      <div class="flex justify-end mb-6">
         <button
           type="button"
           class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
